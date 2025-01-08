60e3edcbb8f9fbafb4aeea30415ac08e93391bf0
Xavier Akers <x-akers@hotmail.com>
2025-01-08 17:01:25 -0500

    Completed documentation for BTNode.java.

--- src/main/java/com/xakers/datastructures/bintree/BTNode.java
+++ src/main/java/com/xakers/datastructures/bintree/BTNode.java
@@ -1,14 +1,30 @@
 package main.java.com.xakers.datastructures.bintree;
 
+/**
+ * Represents a generic node in a binary tree structure.
+ * This interface define the basic behavior for both internal nodes
+ * and leaf nodes in a spatial binary tree.
+ * <p>
+ * Implementing classes should specify whether then ode is a leaf
+ * and provide a method to print or format the node's details.
+ *
+ * @author Xavier Akers
+ * @version 2025-01-06
+ * @since 2025-01-06
+ */
 public interface BTNode {
 
     /**
-     * @return Returns true if it is a leaf node
+     * Determined if the current node is a leaf node.
+     *
+     * @return {@code true} if the node is a leaf, {@code false} otherwise.
      */
-    public boolean isLeaf();
+    boolean isLeaf();
 
     /**
-     * Node formatting
+     * Prints or formats the current node's details.
+     * Implementing classes should define the specific of how
+     * the node's information is presented.
      */
-    public void print();
+    void print();
 }
